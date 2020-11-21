5e4e603f4dedece883ed680a11d5c3ac18932f5a
arthurChen <cqh0718@gmail.com>
2020-11-21 13:49:40 +0800

    添加使用栈实现树的前中后序遍历

--- tree_demo/binary_tree_demo1.py
+++ tree_demo/binary_tree_demo1.py
@@ -49,12 +49,102 @@ def post_order_traversal(node):
     return node
 
 
+def pre_order_traversal_with_stack(node):
+    """
+    使用栈完成前序遍历
+    :param node:
+    :return:
+    """
+    # stack = list()
+    # while node is not None or len(stack) > 0:
+    #     while node is not None:
+    #         print(node.data)
+    #         stack.append(node)
+    #         node = node.left
+    #     if len(stack) > 0:
+    #         node = stack.pop()
+    #         node = node.right
+
+    res = list()
+
+    if node is None:
+        return res
+
+    stack = list()
+    stack.append(node)
+    while len(stack) != 0:
+        node = stack.pop()
+        res.append(node.data)
+        if node.right is not None:
+            stack.append(node.right)
+        if node.left is not None:
+            stack.append(node.left)
+    print(res)
+
+
+def in_order_traversal_with_stack(node):
+    """
+    使用栈完成中序遍历
+    :param node:
+    :return:
+    """
+    # stack = list()
+    # while node is not None or len(stack) > 0:
+    #     while node is not None:
+    #         stack.append(node)
+    #         node = node.left
+    #     if len(stack) > 0:
+    #         node = stack.pop()
+    #         print(node.data)
+    #         node = node.right
+
+    res = list()
+    if node is None:
+        return res
+
+    stack = list()
+
+    while node or stack:
+        if node:
+            stack.append(node)
+            node = node.left
+        else:
+            node = stack.pop()
+            res.append(node.data)
+            node = node.right
+    print(res)
+
+
+def post_order_traversal_with_satck(node):
+    """
+    使用栈完成后序遍历
+    一个列表存遍历对象
+    一个列表保存结果
+    :param node:
+    :return:
+    """
+    stack = list()
+    result = list()
+    stack.append(node)
+    while len(stack) != 0:
+        node = stack.pop()
+        result.append(node.data)
+        if node.left is not None:
+            stack.append(node.left)
+        if node.right is not None:
+            stack.append(node.right)
+    print(result[::-1])
+
+
 if __name__ == '__main__':
     data_list = [3, 2, 9, None, None, 10, None, None, 8, None, 4]
     root = create_binary_tree(data_list)
     print('前序遍历: ')
-    pre_order_traversal(root)
+    pre_order_traversal_with_stack(root)
+    # pre_order_traversal(root)
     print('中序遍历: ')
-    in_order_traversal(root)
+    in_order_traversal_with_stack(root)
+    # in_order_traversal(root)
     print('后序遍历: ')
-    post_order_traversal(root)
+    # post_order_traversal(root)
+    post_order_traversal_with_satck(root)
